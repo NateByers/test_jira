ed581daa77896edd024ce6e7631ba58947b65dd0
Nathan Byers <natebyers@gmail.com>
2024-01-30 16:58:44 -0500

    Update push_jira_bug_card.py

--- python/push_jira_bug_card.py
+++ python/push_jira_bug_card.py
@@ -7,6 +7,8 @@ jira_organization_id = os.environ['JIRA_ORGANIZATION_ID']
 jira_api_key         = os.environ['JIRA_API_KEY'] 
 jira_project_key     = os.environ['JIRA_PROJECT_KEY']
 github_issue_type    = os.environ['GITHUB_ISSUE_TYPE']
+github_issue_title   = os.environ['GITHUB_ISSUE_TITLE']
+github_issue_body    = os.environ['GITHUB_ISSUE_BODY']
 
 print(f"""
 Configuration:
@@ -15,5 +17,7 @@ jira_user_email: {jira_user_email}
 jira_organization_id: {jira_organization_id}
 jira_api_key: {jira_api_key}
 github_issue_type: {github_issue_type}
+github_issue_title: {github_issue_title}
+github_issue_body: {github_issue_body}
 """
 )
